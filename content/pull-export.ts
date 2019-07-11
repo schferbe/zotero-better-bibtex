004e8737c1fa962db42909cbe4668761c0025674
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2019-07-11 21:31:38 +0200

    devonthink

--- content/pull-export.ts
+++ content/pull-export.ts
@@ -5,6 +5,8 @@ const SERVER_ERROR = 500
 const NOT_FOUND = 404
 
 import { Translators } from './translators'
+import { KeyManager } from './key-manager'
+import * as log from './debug'
 
 function displayOptions(request) {
   const isTrue = new Set([ 'y', 'yes', 'true' ])
@@ -97,6 +99,52 @@ Zotero.Server.Endpoints['/better-bibtex/select'] = class {
       return [OK, 'text/plain', await Translators.exportItems(getTranslatorId(translator), displayOptions(request), { items }) ]
     } catch (err) {
       return [SERVER_ERROR, 'text/plain', '' + err]
+    }
+  }
+}
+
+Zotero.Server.Endpoints['/better-bibtex/items'] = class {
+  public supportedMethods = ['GET']
+
+  public async init(request) {
+    if (!request.query || !request.query['']) return [NOT_FOUND, 'text/plain', 'Could not export library: no path']
+
+    try {
+      const [ , lib, keys, translator ] = request.query[''].match(/\/(?:([0-9]+)\/)?(.*)\.([a-z]+)$/i)
+      const libraryID = parseInt(lib || 0) || Zotero.Libraries.userLibraryID
+      const itemKeys = keys.split(/[\s+]/)
+
+      for (const mode of ['bibtex', 'keys', 'ids']) {
+        let items
+        try {
+          switch (mode) {
+            case 'bibtex':
+              items = KeyManager.keys.find({ citekey: { $in: itemKeys }, libraryID })
+              if (items.length) items = await Zotero.Items.getAsync(items.map(item => item.itemID))
+              break
+
+            case 'keys':
+              items = itemKeys.map(key => Zotero.Items.getByLibraryAndKey(libraryID, key))
+              break
+
+            case 'ids':
+              items = await Zotero.Items.getAsync(itemKeys)
+              break
+          }
+
+        } catch (err) {
+          log.error('/better-bibtex/items', mode, request.query[''], err)
+          continue
+        }
+
+        if (items.length === itemKeys.length) return [ OK, 'text/plain', await Translators.exportItems(getTranslatorId(translator), displayOptions(request), { items }) ]
+      }
+
+      return [NOT_FOUND, 'text/plain', `Could not find items for ${request.query['']}`]
+
+    } catch (err) {
+      return [SERVER_ERROR, 'text/plain', '' + err]
+
     }
   }
 }
